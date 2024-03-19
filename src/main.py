b76bccdbad2c5ca2b1f964a3ea89a8bbe3c10bde
Jose Garzon <jmgarzonv@outlook.com>
2024-03-18 21:30:55 -0500

    FEAT: Implement Constructive Method for Initial Solutions
    Major Changes:
    - Added 'constructive_method' function to generate initial solutions for the TOPTW problem.
    - Implemented 'feasible_path' function to assess the feasibility of inserting a given node into a route.
    - Introduced 'profit_density' as a selection criterion to determine the best insertion node.
    
    Minor Changes:
    - Updated the test suite and included 'test_feasible_route'.
    - Applied Black formatter for consistency.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
+import time
 
 INSTANCES_PATH = r"instances\pr01_10"
 
@@ -93,10 +94,12 @@ def read_instances(folder_path):
 
 class TOPTWSolver:
     def __init__(self, instance):
-        self.node_count = instance.get("N")
+        self.nodes_count = instance.get("N")
         self.filename = instance.get("filename")
         self.points_df = instance.get("points")
         if self.points_df.shape[0] > 1:
+            self.points_df["path"] = None
+            self.points_df.loc[0, "path"] = "all"
             self.Tmin = self.points_df.iloc[0]["opening_time"]
             self.Tmax = self.points_df.iloc[0]["closing_time"]
             self.distance_matrix = (
@@ -144,6 +147,110 @@ class TOPTWSolver:
 
         return upper_bound
 
+    def feasible_path(self, S, new_node_index, path_index, insertion_position):
+        is_feasible = False
+        valid_time_window = False
+        path_time = self.Tmin  # Initialize current time with Tmin
+
+        # Insert the new node into the path at the specified position
+        path = S[path_index].copy()
+        path.insert(insertion_position, new_node_index)
+
+        # Calculate the total time considering travel and service durations
+        for i in range(len(path) - 1):
+            current_node_index = path[i]
+            next_node_index = path[i + 1]
+            print(f"\nNode: {current_node_index} To: {next_node_index}:")
+            # Access distance matrix and duration information
+            t_ij = self.distance_matrix[current_node_index][next_node_index]
+            print(f"Traveling time: {t_ij}")
+            duration = self.points_df.loc[current_node_index, "duration"]
+            print(f"Service time: {duration}")
+
+            # Update current time
+            path_time += duration + t_ij
+            print(f"Path time: {path_time}")
+            next_node_opening_time = self.points_df.loc[next_node_index, "opening_time"]
+            next_node_closing_time = self.points_df.loc[next_node_index, "closing_time"]
+            print(f"Closing time: {next_node_closing_time}")
+            print(f"Openning time: {next_node_opening_time}")
+            if path_time < next_node_closing_time:
+                print("Valid TW")
+                valid_time_window = True
+                if path_time <= next_node_opening_time:
+                    opening_time_difference = next_node_opening_time - path_time
+                    path_time += opening_time_difference
+            else:
+                valid_time_window = False
+                break
+
+        # Check if the total time does not exceed Tmax
+        if path_time <= self.Tmax and valid_time_window:
+            print("Feasible")
+            is_feasible = True
+
+        return is_feasible, path_time
+
+    def profit_density(
+        self, S, new_node_index, path_index, insertion_position, path_time
+    ):
+        # Insert the new node into the path at the specified position
+        path = S[path_index].copy()
+        path.insert(insertion_position, new_node_index)
+        profit_sum = self.points_df.loc[path, "profit"].sum()
+        profit_density = profit_sum / path_time
+        return profit_density
+
+    def constructive_method(self, criteria, paths_count=1, solutions_count=10):
+        start_end = [0, 0]
+        S = [start_end[:] for _ in range(paths_count)]
+        stop = False
+        while stop == False:
+            stop = True
+            best_criteria = 0
+            for index, row in self.points_df.iterrows():
+                if pd.isnull(row["path"]):
+                    for path in range(paths_count):
+                        # print(f"Path: {path}")
+                        for pos in range(1, len(S[path])):
+                            # print(f"Pos: {pos}")
+                            is_feasible, path_time = self.feasible_path(
+                                S, index, path, pos
+                            )
+                            if is_feasible:
+                                path_criteria = criteria(S, index, path, pos, path_time)
+                                print(
+                                    f"Feasible: {is_feasible} , Criteria: {path_criteria}"
+                                )
+                                if path_criteria > best_criteria:
+                                    best_criteria = path_criteria
+                                    best_insertion = {
+                                        "index": index,
+                                        "path": path,
+                                        "position": pos,
+                                        "time": path_time,
+                                    }
+                                    stop = False  # Found another solution, let's try to find a better one
+            if stop == False:
+                S[best_insertion["path"]].insert(
+                    best_insertion["position"], best_insertion["index"]
+                )
+                # print(S)
+                # print(best_insertion["time"])
+                self.points_df.loc[best_insertion["index"], "path"] = best_insertion[
+                    "path"
+                ]
+
+        solution = {
+            "paths": S,
+            "profit": sum(
+                self.points_df.loc[index, "profit"]
+                for sublist in S
+                for index in sublist
+            ),
+        }
+        return solution
+
 
 if __name__ == "__main__":
     instances = read_instances(INSTANCES_PATH)
@@ -151,13 +258,15 @@ if __name__ == "__main__":
     count = 1
     for instance in instances:
         solver = TOPTWSolver(instance)
-        print("Filename:", solver.filename)
-        print(f"Nodes: {solver.node_count}")
-        print(f"Points received: {solver.points_df.shape[0]}")
-        print(f"T min: {solver.Tmin}")
-        print(f"T max: {solver.Tmax}")
-        print(f"Upper bound: {solver.upper_bound}")
 
-        break
+        print("Filename:", solver.filename)
+        print(
+            f"Nodes: {solver.nodes_count}\t Points received: {solver.points_df.shape[0]}"
+        )
+        print(f"Sample:\n {solver.points_df.head(2)}\n")
+        print(f"T min: {solver.Tmin}\tT max: {solver.Tmax}")
+        print(f"Upper bound: {solver.upper_bound}")
+        solution = solver.constructive_method(solver.profit_density, 2)
+        print("Profit: ", solution["profit"])
         count += 1
     print("Total instances: ", count)
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,31 +4,36 @@ import pytest
 import os
 from src.main import read_instances, TOPTWSolver, INSTANCES_PATH
 
+
 @pytest.fixture
 def sample_instance():
     return {
         "filename": "sample_instance.txt",
         "N": 4,
-        "points": pd.DataFrame({
-            "i": [0, 1, 2, 3],
-            "x": [0, 0, 1, 1],
-            "y": [0, 1, 0, 1],
-            "duration": [0, 1, 2, 3],
-            "profit": [10, 20, 30, 40],
-            "opening_time": [0, 0, -2, 11],
-            "closing_time": [10, 10, -1, 20]
-        }).set_index("i")
+        "points": pd.DataFrame(
+            {
+                "i": [0, 1, 2, 3],
+                "x": [0, 0, 1, 1],
+                "y": [0, 1, 0, 1],
+                "duration": [0, 1, 2, 3],
+                "profit": [0, 20, 30, 40],
+                "opening_time": [0, 0, -2, 11],
+                "closing_time": [10, 10, -1, 20],
+            }
+        ).set_index("i"),
     }
 
-def test_TOPTWSolver(sample_instance):
+
+def test_TOPTWSolver_init(sample_instance):
     solver = TOPTWSolver(sample_instance)
-    assert solver.node_count == 4
+    assert solver.nodes_count == 4
     assert solver.filename == "sample_instance.txt"
     assert isinstance(solver.points_df, pd.DataFrame)
     assert solver.points_df.shape[0] == 4
     assert solver.Tmin == 0
     assert solver.Tmax == 10
-    assert solver.upper_bound == 30
+    assert solver.upper_bound == 20
+
 
 def test_distance_matrix(sample_instance):
     solver = TOPTWSolver(sample_instance)
@@ -44,9 +49,6 @@ def test_distance_matrix(sample_instance):
     # Check if the computed distance matrix matches the expected one
     np.testing.assert_allclose(solver.distance_matrix, expected_dist_matrix)
 
-def test_upper_bound(sample_instance):
-    solver = TOPTWSolver(sample_instance)
-    assert solver.upper_bound == 30
 
 def test_read_instances():
     instances = read_instances(INSTANCES_PATH)
@@ -55,3 +57,11 @@ def test_read_instances():
     assert all("filename" in instance for instance in instances)
     assert all("points" in instance for instance in instances)
     assert all(isinstance(instance["points"], pd.DataFrame) for instance in instances)
+
+
+def test_feasible_rute(sample_instance):
+    solver = TOPTWSolver(sample_instance)
+    solution = solver.constructive_method(solver.profit_density, 2)
+    assert solution["paths"][0] == [0, 1, 0]
+    assert solution["paths"][1] == [0, 0]
+    assert solution["profit"] == 20
